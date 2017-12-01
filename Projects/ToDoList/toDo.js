a903cdab817d727c82d20f48d29fbbcfc4de9406
Jacob Geiser <32041734+JGeiser9@users.noreply.github.com>
2017-12-01 04:21:32 -0600

    Update toDo.js

--- Projects/ToDoList/toDo.js
+++ Projects/ToDoList/toDo.js
@@ -1,3 +1,4 @@
+//Looping through the list and running the completedTask function on click for each list item
 const listItem = document.getElementById("list").getElementsByTagName('li');
     for (let i = 0; i < listItem.length; i++) {
         listItem[i].addEventListener('click', completedTask);
@@ -12,6 +13,7 @@ function completedTask() {
     }
 }
 
+//Create a new task on the to do list and submit with enter key
 const addNewTask = document.querySelector("input.addNewTask");
     addNewTask.addEventListener('keypress', (event) => {
         let key = event.which || event.keyCode; //looking for numerical key values
@@ -22,4 +24,15 @@ const addNewTask = document.querySelector("input.addNewTask");
             ul.appendChild(li); //adds new li to the list
             addNewTask.value = addNewTask.defaultValue; //resets to placeholder txt 
         } 
+    });
+
+//Toggling the input area for a new task by clicking on the plus icon
+const toggleInput = document.querySelector("i.fa-plus");
+const addNewToDo = document.querySelector("input.addNewTask")
+    toggleInput.addEventListener('click', () => {
+      if (addNewToDo.style.display == 'none') {
+         addNewToDo.style.display = 'block'; 
+      } else {
+        addNewToDo.style.display = 'none';   
+      }
     });
