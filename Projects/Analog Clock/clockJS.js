4dfdc742fa049c09d1a4a93cfc0cd079f565ae36
Jacob Geiser <32041734+JGeiser9@users.noreply.github.com>
2017-12-06 00:30:52 -0600

    Update clockJS.js

--- Projects/Analog Clock/clockJS.js
+++ Projects/Analog Clock/clockJS.js
@@ -7,15 +7,20 @@ setInterval(function(){
 }, 1000);
 
 function updateClock(date){
+  //Set variables to corresponding ID's
   const secHand = document.getElementById("second-hand").style,
         minHand = document.getElementById("minute-hand").style,
         hrHand = document.getElementById("hour-hand").style;
   
+  //Setting the hands to transform with the conversion to degrees
   secHand.transform = "rotate(" + date.getSeconds() * 6 + "deg)";
   minHand.transform = "rotate(" + date.getMinutes() * 6 + "deg)";
   hrHand.transform = "rotate(" + (date.getHours() * 30 + date.getMinutes() * 0.5) + "deg)";
 }
 
+//returns the given time 
 function updateTime(date){
-  date.getHours() + date.getMinutes() + date.getSeconds();
-}
+  const time = date.getHours() + date.getMinutes() + date.getSeconds();
+  return time;
+} 
+
