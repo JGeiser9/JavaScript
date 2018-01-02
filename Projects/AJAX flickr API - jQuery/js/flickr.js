01f3253c8aae9d909527c86cdd81071fdbce5947
Jacob Geiser <jageiser9@gmail.com>
2018-01-01 21:10:28 -0600

    Create flickr.js

---
+++ Projects/AJAX flickr API - jQuery/js/flickr.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+ $('button').click(function () {
+    $("button").removeClass("selected"); // remove "selected" from all buttons
+    $(this).addClass("selected"); // add "selected" and styling to clicked
+
+    // AJAX
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    var animal = $(this).text(); // grabs the text within the button tag for the search
+    var flickrOptions = {
+      tags: animal,
+      format: "json"
+    }; // customizing the search based on which button is clicked
+    function displayPhotos(data) {
+      var photoHTML = '<ul>';
+      $.each(data.items,function(i,photo) {
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      }); // end each
+      photoHTML += '</ul>';
+      $('#photos').html(photoHTML); // printing the string to the "photos" div
+    }
+    $.getJSON(flickerAPI, flickrOptions, displayPhotos); // sending the request
+
+  }); // end click
+
+}); // end ready
