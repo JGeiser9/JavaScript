22fc8fc49952db0b6e44d454323860146e173066
Jacob Geiser <jageiser9@gmail.com>
2018-01-01 21:15:20 -0600

    Update flickr.js

--- Projects/AJAX flickr API - jQuery/js/flickr.js
+++ Projects/AJAX flickr API - jQuery/js/flickr.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     }; // customizing the search based on which button is clicked
     function displayPhotos(data) {
       var photoHTML = '<ul>';
+      // loop through each item in the JSON items object
       $.each(data.items,function(i,photo) {
         photoHTML += '<li class="grid-25 tablet-grid-50">';
         photoHTML += '<a href="' + photo.link + '" class="image">';
