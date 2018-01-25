347f4266078a80a8971fd5dd3cfc2943e96231fa
Jacob Geiser <jageiser9@gmail.com>
2018-01-25 01:24:50 -0600

    Update app.js

--- Node.js/Retrieve Profile Information/app.js
+++ Node.js/Retrieve Profile Information/app.js
@@ -1,37 +1,6 @@
-// Need a way to look at a user's badge count and JavaScript points
-// Connect to Treehouse's API to get profile information to print out to console
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
-// Reqiure https
-const https = require('https');
-
-// Function to print out the message to the console
-function printMessage(username, badgeCount, point) {
-  const message = `${username} has ${badgeCount} total badge(s) and ${point} points in JavaScript;`
-  console.log(message);
-}
-
-function getProfile(username) {
-  
-  // Connect to the API URL (https://teamtreehouse.com/username.json) with https request
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    // console.dir(response.statusCode); Returns if the connection was successful or not
-    let body = "";
-    
-    // Read the data
-    response.on('data', data => {
-      body += data.toString();
-    });
-   
-    response.on('end', () => {
-        // Parse the data
-        const profile = JSON.parse(body);
-        //console.dir(profile); This shows you the profile object containing all profile information
-      
-        // Print the data to the console with the printMessage function
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-    });
-  });
-}
-
+const profile = require('./profile.js'); // Requiring my own file (./ is mandatory)
 const users = process.argv.slice(2); // The process.argv property returns an array containing the command line arguments passed 
-users.forEach(getProfile); // Loop through each array element and get the profile of the username passed through Node.js Command Line
+users.forEach(profile.get); // Loop through each array element and call the getProfile function from profile.js
