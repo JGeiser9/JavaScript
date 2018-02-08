b97b4df810cd83476a44dfbb62f04f88d0712dd2
Jacob Geiser <jageiser9@gmail.com>
2018-02-08 15:59:33 -0600

    Updated to grab the most recent badge earned

--- Node.js/Retrieve Treehouse Profile/router.js
+++ Node.js/Retrieve Treehouse Profile/router.js
@@ -19,7 +19,7 @@ function home(request, response) {
   if(request.url === "/") {
     if(request.method.toLowerCase() === "get") {
       //show search
-      response.writeHead(200, commonHeaders);  
+      response.writeHead(200, commonHeaders);
       renderer.view("header", {}, response);
       renderer.view("search", {}, response);
       renderer.view("footer", {}, response);
@@ -27,15 +27,15 @@ function home(request, response) {
     } else {
       //if url == "/" && POST
         // get the post data from body
-          request.on("data", function(postBody) {
-            // extract the username from the ost query string
-              var query = querystring.parse(postBody.toString());
-              //redirect to /:username (Give a 303 code - force GET request to new url even if it was a POST request)
-                response.writeHead(303, {"Location": "/" + query.username});
-                response.end();
-          })
+        request.on("data", function(postBody) {
+          // extract the username from the ost query string
+          var query = querystring.parse(postBody.toString());
+          //redirect to /:username (Give a 303 code - force GET request to new url even if it was a POST request)
+          response.writeHead(303, {"Location": "/" + query.username});
+          response.end();
+        })
+      }
     }
-  } 
 }
 
 //Handle HTTP route GET /:username i.e. /jake
@@ -43,29 +43,31 @@ function user(request, response) {
   //if url == "/...."
   var username = request.url.replace("/", "");
   if(username.length > 0 && request.url.indexOf('.css') === -1) {
-    response.writeHead(200, commonHeaders);  
-    renderer.view("header", {}, response);    
-    
+    response.writeHead(200, commonHeaders);
+    renderer.view("header", {}, response);
+
     //get json from Treehouse
     var studentProfile = new Profile(username);
-    
+
     //on "end"
     studentProfile.on("end", function(profileJSON){
       //show profile
       //Store the values which we need
       var values = {
-        avatarUrl: profileJSON.gravatar_url, 
+        avatarUrl: profileJSON.gravatar_url,
         username: profileJSON.profile_name,
         badges: profileJSON.badges.length,
-        javascriptPoints: profileJSON.points.JavaScript
+        javascriptPoints: profileJSON.points.JavaScript,
+        recentBadge: profileJSON.badges[profileJSON.badges.length-1].name,
+        badgeIcon: profileJSON.badges[profileJSON.badges.length-1].icon_url
       }
-      
+
       //Simple response
       renderer.view("profile", values, response);
       renderer.view("footer", {}, response);
       response.end();
     });
-        
+
     //on "error"
     studentProfile.on("error", function(error){
       //show error
@@ -74,10 +76,6 @@ function user(request, response) {
       renderer.view("footer", {}, response);
       response.end();
     });
-      
+
   }
 }
-
-module.exports.home = home;
-module.exports.user = user;
-module.exports.style = style;
